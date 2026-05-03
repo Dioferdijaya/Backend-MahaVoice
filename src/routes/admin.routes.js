a945047f4f223069987bc5ea9ddf18825b10e006
Dio Ferdi Jaya <email@usk.ac.id>
2026-05-03 10:36:27 +0700

    feat(routes): add admin routes for screening history

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const { getAdminDashboard, saveScreeningResult } = require('../controllers/admin.controller');
+const {
+	getAdminDashboard,
+	saveScreeningResult,
+	getAdminScreeningQuestions,
+	createScreeningQuestion,
+	updateScreeningQuestion,
+	deleteScreeningQuestion,
+	getAdminScreeningResults,
+	getAdminUsers,
+} = require('../controllers/admin.controller');
 
 const router = express.Router();
 
 router.get('/dashboard', getAdminDashboard);
 router.post('/screenings', saveScreeningResult);
+router.get('/screening-results', getAdminScreeningResults);
+router.get('/users', getAdminUsers);
+router.get('/screening-questions', getAdminScreeningQuestions);
+router.post('/screening-questions', createScreeningQuestion);
+router.put('/screening-questions/:id', updateScreeningQuestion);
+router.delete('/screening-questions/:id', deleteScreeningQuestion);
 
 module.exports = router;
